7abcf736b484d232baff58844581b4832611b825
Imple Lee <80144331+ImpleLee@users.noreply.github.com>
2022-08-30 22:19:14 +0800

    load CCloader in main.lua
    In order to know the error message better

--- main.lua
+++ main.lua
@@ -244,6 +244,10 @@ then
     MES.new('error',"An error occured during loading, and some data was lost.")
 end
 
+
+require'CCloader'
+
+
 --Initialize fields, sequence, missions, gameEnv for cutsom game
 local fieldData=loadFile('conf/customBoards','-string -canSkip')
 if fieldData then
